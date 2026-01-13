8bfc528d8e6c979b63461a8053c70a5154d07abe
ss0379182@gmail.com <108196088+shahin200131@users.noreply.github.com>
2026-01-13 15:12:49 +0600

    Prepare Django for production deployment

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Navigate to the Django project directory
+cd animalaid-server
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run database migrations
+python manage.py migrate
